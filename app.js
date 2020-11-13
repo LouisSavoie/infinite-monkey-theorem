470ad7314bc0fa7a7a9b44633c5e64b1b6489b6a
Louis Savoie <thelousavoie@gmail.com>
2020-11-12 21:56:47 -0500

    add easy mode

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const generateButton = document.querySelector('#generateButton');
 
 // CHARACTER SETS
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\',.?!;:()-                    ';
+const charactersEasy = 'abcdefghijklmnopqrstuvwxyz      ';
 
 // GENERATION FUNCTIONS
 function generate(length) {
@@ -13,9 +14,21 @@ function generate(length) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
-}
+};
+
+function generateEasy(length) {
+    let result = '';
+    for(let i = 0; i < length; i++) {
+        result += charactersEasy.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
 
 // CLICK EVENTS
 generateButton.addEventListener("click", function() {
     textDisplay.innerHTML = generate(lengthInput.value);
+});
+
+generateEasyButton.addEventListener("click", function() {
+    textDisplay.innerHTML = generateEasy(lengthInput.value);
 });
